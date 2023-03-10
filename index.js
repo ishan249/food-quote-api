074d6f42e8026f673eb6942f6d9b7adfcb4fa04c
ishan249 <249patelishan@gmail.com>
2023-03-11 00:15:49 +0530

    added / route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 app.use(cors());
 const quotes = require("./quotes.js");
 
+app.get("/", (req,res)=>{
+    res.send("Hello I am food quote generator api. Please got to route /api/foodquote to generate response")
+})
+
+
 app.get("/api/foodquote", (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   res.send(quotes[randomIndex]);
