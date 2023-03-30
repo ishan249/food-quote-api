1940f660d3993a11570a321ef3eb0077b9472d69
ishan249 <249patelishan@gmail.com>
2023-03-30 15:52:04 +0530

    changed some code

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+const axios= require("axios");
 const quotes = require("./quotes.js");
 
 app.get("/", (req,res)=>{
     res.send("Hello I am food quote generator api. Please got to route /api/foodquote to generate response")
 })
-
 
 app.get("/api/foodquote", (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
